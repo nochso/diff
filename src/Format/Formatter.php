4046a643688deb035d5672249a35f7781676e081
Marcel Voigt <mv@noch.so>
2016-03-15 17:49:10 +0100

    refactor: Add interface Formatter

---
+++ src/Format/Formatter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace nochso\Diff\Format;
+
+interface Formatter
+{
+    /**
+     * Format an array diff.
+     *
+     * @param mixed[][] $diff     The result of Differ::diffToArray
+     * @param string[]  $messages Optional array of messages or warnings.
+     *
+     * @return mixed
+     */
+    public function format($diff, $messages = []);
+}
