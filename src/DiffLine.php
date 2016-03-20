f64a9a1759b3418d15beebcf8a1ccf01daa5332e
Marcel Voigt <mv@noch.so>
2016-03-20 13:07:29 +0100

    refactor: Make DiffLine an actual object. Used by Diff.

--- src/DiffLine.php
+++ src/DiffLine.php
@@ -2,20 +2,66 @@
 namespace nochso\Diff;
 
 /**
- * DiffLine holds constants to index positions in a diff line.
- *
- * @todo Make this a usable object for templates and a common API? See ContextDiff.
+ * DiffLine holds a single line of a diff.
  */
 class DiffLine
 {
     // Basic items as returned by Differ::diffToArray
     const TEXT = 0;
     const ACTION = 1;
-
     // Additional info provided by ContextDiff
     const LINE_NUMBER_FROM = 2;
 
-    private function __construct()
+    /**
+     * @var array
+     */
+    private $line;
+
+    /**
+     * @return int
+     */
+    public function getLineNumberFrom()
     {
+        return $this->line[self::LINE_NUMBER_FROM];
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->line[self::TEXT];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAddition()
+    {
+        return $this->line[self::ACTION] === Differ::ADD;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSame()
+    {
+        return $this->line[self::ACTION] === Differ::SAME;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRemoval()
+    {
+        return $this->line[self::ACTION] === Differ::REMOVE;
+    }
+
+    /**
+     * @param mixed[] $diffLine
+     */
+    public function __construct($diffLine)
+    {
+        $this->line = $diffLine;
     }
 }
