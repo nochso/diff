dc1d371900b78ba72af4897493cc2ed1a88d6aff
Marcel Voigt <mv@noch.so>
2016-03-23 22:43:56 +0100

    docs: Improve Formatter interface phpdocs

--- src/Format/Formatter.php
+++ src/Format/Formatter.php
@@ -3,12 +3,15 @@ namespace nochso\Diff\Format;
 
 use nochso\Diff\Diff;
 
+/**
+ * Formatter interface implemented by templates and anything that can render a Diff.
+ */
 interface Formatter
 {
     /**
      * Format a Diff object.
      *
-     * @param \nochso\Diff\Diff $diff
+     * @param \nochso\Diff\Diff $diff The diff to be formatted / rendered.
      *
      * @return mixed
      */
